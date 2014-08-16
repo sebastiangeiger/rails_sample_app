06256eb7947b778e84aa381b236b12a6465853c8
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-08-15 21:37:39 -0400

    Creating a message class

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Message.create(content: "Hello ther!")
+Message.create(content: "The Database is obviously working")
+Message.create(content: "That's good.")
+
